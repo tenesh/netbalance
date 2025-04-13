de8cf89a52ac691460146c15d42be4b1bf94b7fe
tenesh <teneshdev@gmail.com>
2025-04-13 21:16:56 +0800

    wip

--- resources/js/layouts/AuthLayout.tsx
+++ resources/js/layouts/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-    return <main className="flex h-full w-full">{children}</main>;
+    return <main className="flex h-screen w-full">{children}</main>;
 };
 
 export default AuthLayout;
